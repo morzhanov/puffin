47bfa305521d40faa6baa38a66475ec830229de4
VladislavMorzhanov <morzhanov.vladislav@steelkiwi.com>
2018-04-16 00:16:31 +0300

    Added adge arrows

--- src/components/FullSize.jsx
+++ src/components/FullSize.jsx
@@ -4,6 +4,7 @@ import Paper from 'material-ui/Paper'
 import ArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left'
 import ArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right'
 import styled from 'styled-components'
+// import Loader from './Loader'
 
 const Wrapper = styled.div`
   position: absolute;
@@ -17,16 +18,36 @@ const Wrapper = styled.div`
   align-items: center;
 `
 
+const ArrowPanel = styled.div`
+  background-color: rgba(98,98,98,0.46);
+  width: 100px;
+  position: fixed;
+  top: 0;
+  left: ${({ left }) => left ? 0 : 'auto'};
+  right: ${({ left }) => left ? 'auto' : 0};
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  cursor: pointer;
+  transition: all ease 300ms;
+  .arrow {
+    width: 70px !important;
+    height: 70px !important;
+    color: rgba(174,174,174,0.75) !important;
+    &:hover {
+      color: rgba(174,174,174,0.98) !important;
+    }
+  }
+  &:hover {
+    background-color: rgba(98,98,98,0.61);
+  }
+`
+
 const Image = styled.img`
   width: auto;
   height: 80%;
 `
-
-const arrowStyles = {
-  width: 50,
-  height: 50,
-  cursor: 'pointer'
-}
 
 class FullSize extends React.Component {
   componentDidMount () {
@@ -40,15 +61,15 @@ class FullSize extends React.Component {
   render () {
     const {rootStore} = this.props
     return <Wrapper>
-      <ArrowLeft className="arrow"
-        onClick={this.prev}
-        style={arrowStyles}/>
+      <ArrowPanel left>
+        <ArrowLeft className="arrow" onClick={this.prev}/>
+      </ArrowPanel>
       <Paper zDepth={4}>
         <Image src={rootStore.currentPhoto}/>
       </Paper>
-      <ArrowRight className="arrow"
-        onClick={this.next}
-        style={arrowStyles}/>
+      <ArrowPanel>
+        <ArrowRight className="arrow" onClick={this.next}/>
+      </ArrowPanel>
     </Wrapper>
   }
 }
